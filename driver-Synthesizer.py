e1c2a3d8e4c1e3d9f7df361cf4adb5afe50e3488
Jacob Bullard <u0625839@gcloud.utah.edu>
2023-01-31 21:56:45 -0700

    updated object and function names

--- driver-Synthesizer.py
+++ driver-Synthesizer.py
@@ -4,27 +4,41 @@ one that synthesizes a plain text file.
 This is authored by Jacob Bullard Computer Security Spring 2023"""
 
 
-from Synthesizer import count_them_thangs
+from Synthesizer import deCode
 
 # In the beginning
-what_is_this = open("cipher_text.txt", "rb") # opening for [r]eading as [b]inary
-data = what_is_this.read() # if you only wanted to read 512 bytes, do .read(512)
+gibberish = open("cipher_text.txt", "rb") # opening for [r]eading as [b]inary
+encoded_data = gibberish.read() # if you only wanted to read 512 bytes, do .read(512)
 
 # Between now and the beginning, I've been using StackOverFlow.
 # .....and running into errors
 # Let's hope this thread is actually helpful. 
 # https://stackoverflow.com/questions/6787233/python-how-to-read-bytes-from-file-and-save-it
 
-num_classification_of_thangs =len(count_them_thangs(data))
-print(str(num_classification_of_thangs) + " diffrant thangs found\n")
+num_bytes =len(deCode(encoded_data))
 
-print(count_them_thangs(data))
+cipher_key = "SPWOEKFMBZGDXVIYACQLHJRTUN"
+frequency_key = "ETAOINSRHDLUCMFYWGPBVKXQJZ"
+
+decoded_message = ""
+
+print(cipher_key[0])
+print(frequency_key[0])
+
+print(str(num_bytes) + " diffrant thangs found\n")
+
+print(str(encoded_data))
+
+print()
 
 population_size = 0
+byte_list = []
+for byte in deCode(encoded_data):
+    byte_list.append(str(byte))
+    population_size = population_size + deCode(encoded_data).get(byte)
+    print("Byte: " + str(byte) + " was detected " + str(deCode(encoded_data).get(byte)) + " times.")
 
-for thang in count_them_thangs(data):
-    population_size = population_size + count_them_thangs(data).get(thang)
-    print("This thang: " + str(thang) + " was spotted " + str(count_them_thangs(data).get(thang)) + " particular times.")
+print()
 
 """Assemble them all together now and this defines
 our population/domain_space-ish/physical_to_neuro"""
@@ -54,7 +68,13 @@ block_text = """
 #print(block_text) - wow it works
 print("Population Size or Sample Amount or Time Duration or Sensor Measurement: " + str(population_size))
 
+print()
+
+cipher_text_decoded = ""
+for i in range(len(byte_list)):
+    cipher_text_decoded = cipher_text_decoded + byte_list[i] + " "
 # 
-# Houston, we are going to need to do a little math here. 
+# Houston, we are going to need a map and a compass, on second thought, forget the compass. 
 # 
-# 
+
+print(cipher_text_decoded)
