cfa5f1ddf59fb1d048cb32ba0b73572dd241fa79
Jacob Bullard <u0625839@gcloud.utah.edu>
2023-01-31 21:57:25 -0700

    updated object and function names

--- Synthesizer.py
+++ Synthesizer.py
@@ -4,15 +4,12 @@
 # 
 from collections import Counter
 
-def count_them_thangs(what_is_this):
+def deCode(_binary_stream):
     # Step 1: Store the # of times we observe/feel-neuro/sense each thang
-    diffrant_thangs = Counter(what_is_this)
+    bytes = Counter(_binary_stream)
 
-    # Step 2: Sort the thangs by popularity
-    ranked_thangs = dict(sorted(diffrant_thangs.items(), key=lambda x: x[1], reverse=True))
+    # Step 2: Sort the bytes by popularity
+    order_bytes = dict(sorted(bytes.items(), key=lambda x: x[1], reverse=True))
 
     # Step 3: Return the sorted frequency count
-    return ranked_thangs
-
-ciphertext = "hllwrdlwryld"
-print(count_them_thangs(ciphertext))
+    return order_bytes
